906d74ce49398962c0c430d2021976d91286bbdb
Tatsuya Hotta <hotterdragon@gmail.com>
2021-10-11 22:12:18 +0900

    リファクタリング

--- src/main/kotlin/io/github/drahot/redis_plugin/RedisDevToolWindowFactory.kt
+++ src/main/kotlin/io/github/drahot/redis_plugin/RedisDevToolWindowFactory.kt
@@ -1,13 +1,12 @@
-package io.github.drahot.intellijredisplugin
+package io.github.drahot.redis_plugin
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 
-class RedisDevToolWindowFactory: ToolWindowFactory {
+class RedisDevToolWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         TODO("Not yet implemented")
     }
-
 }
