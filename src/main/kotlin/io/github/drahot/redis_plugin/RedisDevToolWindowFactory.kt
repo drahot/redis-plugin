1ec675a35b379f74e47377737af96f8b1c4b1974
Tatsuya Hotta <hotterdragon@gmail.com>
2021-10-11 22:11:33 +0900

    RedisDevToolWindowFactoryを追加

---
+++ src/main/kotlin/io/github/drahot/redis_plugin/RedisDevToolWindowFactory.kt
@@ -0,0 +1,13 @@
+package io.github.drahot.intellijredisplugin
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class RedisDevToolWindowFactory: ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        TODO("Not yet implemented")
+    }
+
+}
